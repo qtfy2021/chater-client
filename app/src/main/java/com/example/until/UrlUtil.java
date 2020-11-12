0107d204ffa71534b99cdd3c6dd4784dd326d58c
tukinokage <3355847539@qq.ccom>
2020-11-12 18:27:28 +0800

    准备测试登陆token

--- app/src/main/java/com/example/until/UrlUtil.java
+++ app/src/main/java/com/example/until/UrlUtil.java
@@ -9,6 +9,8 @@ public class UrlUtil {
     public static class API_URL{
         public static final String LOGIN_URL = "http://172.27.35.1:7371/ls/login";
         public static final String TOKEN_LOGIN_URL = "http://172.27.35.1:7371/ls/loginToken";
+       // public static final String REGISTER_URL = "http://172.27.35.1:7371/ls/register";
+
 
     }
 
